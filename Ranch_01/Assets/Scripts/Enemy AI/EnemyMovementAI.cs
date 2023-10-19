216adeff11a9010e1713d9d9cd9017bf343f6ce5
Kuna10000 <113560906+Kuna10000@users.noreply.github.com>
2023-10-18 21:34:16 -0600

    asf

--- Ranch_01/Assets/Scripts/Enemy AI/EnemyMovementAI.cs
+++ Ranch_01/Assets/Scripts/Enemy AI/EnemyMovementAI.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class EnemyMovementAI : MonoBehaviour
 {
-
+    const float minPathUpdateTime = .2f;
+    const float pathUpdateMoveThreshold = .5f;
     public Transform target;
     public float speed = 1;
     Vector3[] path;
     int targetIndex;
 
-    void Update()
+    void Start()
     {
-        PathRequestManager.RequestPath(transform.position, target.position, OnPathFound);
+        StartCoroutine(UpdatePath());
     }
 
     public void OnPathFound(Vector3[] newPath, bool pathSuccessful)
@@ -25,7 +26,28 @@ public class EnemyMovementAI : MonoBehaviour
             StartCoroutine("FollowPath");
         }
     }
+    IEnumerator UpdatePath()
+    {
 
+        if (Time.timeSinceLevelLoad < .3f)
+        {
+            yield return new WaitForSeconds(.3f);
+        }
+        PathRequestManager.RequestPath(transform.position, target.position, OnPathFound);
+
+        float sqrMoveThreshold = pathUpdateMoveThreshold * pathUpdateMoveThreshold;
+        Vector3 targetPosOld = target.position;
+
+        while (true)
+        {
+            yield return new WaitForSeconds(minPathUpdateTime);
+            if ((target.position - targetPosOld).sqrMagnitude > sqrMoveThreshold)
+            {
+                PathRequestManager.RequestPath(transform.position, target.position, OnPathFound);
+                targetPosOld = target.position;
+            }
+        }
+    }
     IEnumerator FollowPath()
     {
         Vector3 currentWaypoint = path[0];
