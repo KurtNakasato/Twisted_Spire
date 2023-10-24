885e183ab485761c21de289071a95e35a43b1bdb
Kuna10000 <113560906+Kuna10000@users.noreply.github.com>
2023-10-23 20:17:42 -0600

    Patrol

--- Ranch_01/Assets/Scripts/Enemy/EnemyPatrolState.cs
+++ Ranch_01/Assets/Scripts/Enemy/EnemyPatrolState.cs
@@ -1,9 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
-
+using System;
 public class EnemyPatrolState : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -11,7 +12,7 @@ public class EnemyPatrolState : MonoBehaviour
     public float speed;
     public Transform[] patrolPoints;
     public float waitTime;
-    int currentPointIndex;
+    int index;
 
     float threshhold;
 
@@ -21,60 +22,35 @@ public class EnemyPatrolState : MonoBehaviour
     private void Start()
     {
         threshhold = .05f;
-        currentPointIndex = 0;
+        index = 0;
+        StartCoroutine(FollowPath());
     }
-
-    private void Update()
+    IEnumerator FollowPath()
     {
-
-        StartCoroutine(Wait());
-        //Debug.Log(Vector3.Distance(transform.position, patrolPoints[currentPointIndex].position));
-        //if (Vector3.Distance(transform.position, patrolPoints[currentPointIndex].position) <= threshhold)
-        //{
-        //    transform.position = Vector2.MoveTowards(transform.position, patrolPoints[currentPointIndex].position, speed * Time.deltaTime);
-        //    Vector2 currentPos = new Vector2(transform.position.x, transform.position.y);
-        //    Vector2 direction = new Vector2(patrolPoints[currentPointIndex].position.x - currentPos.x, patrolPoints[currentPointIndex].position.y - currentPos.y);
-        //    transform.up = direction;
-        //}
-        //else
-        //{
-        //   // Debug.Log("Arrived");
-        //    if (once == false)
-        //    {
-        //        once = true;
-        //        StartCoroutine(Wait());
-        //    }
-        //}
-    }
-
-    IEnumerator Wait()
-    {
-        yield return new WaitForSeconds(waitTime);
-        transform.position = Vector2.MoveTowards(transform.position, patrolPoints[currentPointIndex].position, speed * Time.deltaTime);
-        //yield return null;
-        //Vector2 currentPos = new Vector2(transform.position.x, transform.position.y);
-        //Vector2 direction = new Vector2(patrolPoints[currentPointIndex].position.x - currentPos.x, patrolPoints[currentPointIndex].position.y - currentPos.y);
-        //transform.up = direction;
-
-        if (Vector2.Distance(transform.position, patrolPoints[currentPointIndex].position) <= threshhold) {
-
-
-
-                if (currentPointIndex + 1 < patrolPoints.Length)
-                {
-                    currentPointIndex++;
-                }
-                else
-                {
-                    if (reverse)
-                    {
-                        patrolPoints.Reverse();
-                    }
-                    currentPointIndex = 0;
-                }
+        while (true)
+        {
+            foreach (Transform point in patrolPoints)
+            {
+                yield return StartCoroutine(Move(point));
+            }
+            if (reverse == true)
+            {
+                Array.Reverse(patrolPoints);
+            }
         }
-        yield return null;
-        
+    }
+    IEnumerator Move(Transform point)
+    {
+       yield return new WaitForSeconds(waitTime);
+       while (Vector2.Distance(transform.position, point.position) >= threshhold)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, point.position, speed * Time.deltaTime);
+            Vector2 direction = new Vector2(point.position.x - transform.position.x, point.position.y - transform.position.y);
+            transform.up = direction;
+            yield return null;
+
+        }
+
     }
 
 }
